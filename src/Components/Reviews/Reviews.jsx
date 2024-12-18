14a36b7bbd591855e58eab1d69436311b265d58f
mangianees <mangianees@gmail.com>
2024-12-18 19:07:21 +0000

    Card Scrolling done

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,35 +1,89 @@
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import './Reviews.css'
-import reviewCard from '../../images/reviewCard.png'
-import review1 from '../../images/review01.png'
-import { Container } from 'react-bootstrap';
+import React, { useRef } from "react";
+import Card from "react-bootstrap/Card";
+import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Reviews.css";
+import reviewCard from "../../images/reviewCard.png";
+import review1 from "../../images/review01.png";
 
 function Reviews() {
-  return (
-    <>
-    <Container>
-    <CardGroup>
+  const scrollContainer = useRef(null);
 
-    <Card >
-         <Card.Img variant="top" src= {reviewCard} className='reviewCard'/>
-        
-      </Card>
-      <Card >
-         <Card.Img variant="top" src= {review1} className='reviewCard'/>
-        
-      </Card>
-      <Card>
-         <Card.Img variant="top" src= {review1} className='reviewCard'/>
-        
-      </Card>
-      <Card>
-         <Card.Img variant="top" src= {review1} className='reviewCard'/>
-        
-      </Card>
-    </CardGroup>
+  const handleScroll = (direction) => {
+    if (scrollContainer.current) {
+      const scrollAmount = 240; // Adjust scroll amount as needed
+      scrollContainer.current.scrollBy({
+        left: direction === "left" ? -scrollAmount : scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <Container className="reviews-container">
+      {/* Static Card */}
+      <div className="static-card">
+        <Card>
+          <Card.Img variant="top" src={reviewCard} className="reviewCard" />
+        </Card>
+      </div>
+
+      {/* Scrollable Section */}
+      <div className="scrollable-section">
+        {/* Left Scroll Button */}
+        <button
+          className="scroll-btn left-btn"
+          onClick={() => handleScroll("left")}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+
+        {/* Scrollable Cards */}
+        <div className="scrollable-container" ref={scrollContainer}>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+          <Card className="scrollable-card">
+            <Card.Img variant="top" src={review1} className="reviewCard" />
+          </Card>
+        </div>
+
+        {/* Right Scroll Button */}
+        <button
+          className="scroll-btn right-btn"
+          onClick={() => handleScroll("right")}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+      </div>
     </Container>
-    </>
   );
 }
 
