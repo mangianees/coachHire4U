cbf22d5b3cd705d7826691593663849b483f85fc
mangianees <mangianees@gmail.com>
2024-12-23 12:51:30 +0000

    Modules refactors done

--- src/Components/ThingsToDo/ThingsToDo.jsx
+++ src/Components/ThingsToDo/ThingsToDo.jsx
@@ -1,75 +1,16 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import "./ThingsToDo.css";
-import "../Quote/Quote.css";
-import MyCard from "../Card/MyCard.jsx";
 
-import { useTranslation } from "react-i18next";
-import thingsToDo01 from '../../images/ThingsToDo/thingsToDo1.jpg'
-import thingsToDo02 from '../../images/ThingsToDo/thingsToDo2.jpg'
-import thingsToDo03 from '../../images/ThingsToDo/thingsToDo3.jpg'
+import { CardThingsToDo, VariablesThingsToDo } from "./ThingsToDoModule.jsx";
+import "./ThingsToDoModule.jsx"
 
 
 function ThingsToDo() {
 
-  const { t } = useTranslation();
-  const thingsTitle = "THINGS TO DO";
-  const thingsBody = "If you’re arranging a british history tour for your students or want to show the magic of UK to your corporate clients or colleagues, look no further. We can cater to all your group travel needs and at a fraction of the cost of other transportation options.";
-
-  const economyCardBody =
-    "The ideal solution for all those who are looking for the cheapest coachhire services. All our vehicles comply with safety regulations.";
-  const businessCardBody =
-    "Dedicated to Businesses of all sizes as we have a wide range of vehicles in the business executive class. It's perfect service for no-frills airport transfers of smaller or larger groups.";
-  const vipCardBody =
-    "Luxurious service for special occasions. Whenever you need that exceptional coach, choose our limited edition pack with VIP Class.";
-
-    const card1Title = t("LARMAL FESTIVAL");
-    const card2Title = t("HORSE RACING LONDON");
-    const card3Title = t("MUSEUM AND DAY TRIP");
+  VariablesThingsToDo();
 
   return (
     <>
       <br />
-      <Card>
-        <Container>
-          {/* Header Section */}
-          <Row className="OurFleetHeader">
-            <Col>
-              <h2>{t("thingsTitle")}</h2>
-              <p>
-                {t("thingsBody")}
-              </p>
-              <Button className="myButton"> {t("READ MORE")}</Button>
-            </Col>
-          </Row>
-
-          {/* Cards Section */}
-          <Row className="fleet-cards">
-            <Col md={4} className="card-container">
-              <MyCard cardTitle={t("card1Title")} cardImage={thingsToDo02} />
-              
-              
-            </Col>
-
-            <Col md={4} className="card-container">
-              <MyCard
-                cardTitle={t("card2Title")}
-                cardImage={thingsToDo01}
-              />
-              
-            </Col>
-
-            <Col md={4} className="card-container">
-              <MyCard
-                cardTitle={t("card3Title")}
-                cardImage={thingsToDo03}
-              />
-
-              
-            </Col>
-          </Row>
-        </Container>
-      </Card>
-      
+      {CardThingsToDo()}
     </>
   );
 }
--- src/Components/TrustBanner/TrustBanner.jsx
+++ src/Components/TrustBanner/TrustBanner.jsx
@@ -1,45 +1,11 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-import TrustCard from "../TrustCard/TrustCard.jsx";
-import './TrustBanner.css';
-import { useTranslation } from "react-i18next";
+
+import { TrustBannerCard } from "./TrustBannerModule.jsx";
 
 function TrustBanner() {
-const { t } = useTranslation();
-    const trustCardTitle = "why choose us for your coach hire"
-    const safe = "Passenger safety is our highest priority."
-    const trusted = "We are Trusted by clients that use us again and again."
-    const reliable = "You can trust Coachhire4u, every step of the way."
-    const flexible = "Our depth of resource ensures there is always a solution!"
-    const value = "Our team are passionate in providing you an excellent service at the best value."
-    const easy = "Get your quote in 30 seconds or simply call our friendly team!"
 
   return (
     <>
-      <Card>
-      <Card.Title className="myCardTitle">{t("trustCardTitle")}</Card.Title>
-        <Container>
-          <Row>
-            <Col>
-              <TrustCard title={t("safeTitle")} body={t("safe")} />
-            </Col>
-            <Col>
-              <TrustCard title={t("trustedTitle")} body={t("trusted")}/>
-            </Col>
-            <Col>
-              <TrustCard title={t("reliableTitle")} body={t("reliable")}/>
-            </Col>
-            <Col>
-              <TrustCard title={t("flexibleTitle")} body={t("flexible")}/>
-            </Col>
-            <Col>
-              <TrustCard title={t("valueTitle")} body={t("value")}/>
-            </Col>
-            <Col>
-              <TrustCard title={t("easyTitle")} body={t("easy")}/>
-            </Col>
-          </Row>
-        </Container>
-      </Card>
+      {TrustBannerCard()}
     </>
   );
 }
